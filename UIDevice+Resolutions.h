36a92378e6e2901b6095abac6b51efb9c0ba539f
Daniel Parnell <me@danielparnell.com>
2018-04-25 11:49:02 +1000

    Code cleanup

--- UIDevice+Resolutions.h
+++ UIDevice+Resolutions.h
@@ -11,7 +11,7 @@
 enum {
     UIDeviceResolution_Unknown           = 0,
     UIDeviceResolution_iPhoneStandard    = 1,    // iPhone 1,3,3GS Standard Display  (320x480px)
-    UIDeviceResolution_iPhoneRetina4    = 2,    // iPhone 4,4S Retina Display 3.5"  (640x960px)
+    UIDeviceResolution_iPhoneRetina4     = 2,    // iPhone 4,4S Retina Display 3.5"  (640x960px)
     UIDeviceResolution_iPhoneRetina5     = 3,    // iPhone 5 Retina Display 4"       (640x1136px)
     UIDeviceResolution_iPadStandard      = 4,    // iPad 1,2,mini Standard Display   (1024x768px)
     UIDeviceResolution_iPadRetina        = 5     // iPad 3 Retina Display            (2048x1536px)
